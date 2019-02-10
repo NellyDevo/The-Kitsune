06a48e294188bab29adbc996c90e665621e3d548
Rin Camelia <rcix2010@gmail.com>
2019-02-09 23:17:07 -0600

    added temp art for shape cards

--- src/main/java/kitsunemod/cards/attacks/KitsuneShape.java
+++ src/main/java/kitsunemod/cards/attacks/KitsuneShape.java
@@ -23,7 +23,7 @@ public class KitsuneShape extends AbstractKitsuneCard {
     public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    public static final String IMG_PATH = "kitsunemod/images/cards/strike.png";
+    public static final String IMG_PATH = "kitsunemod/images/cards/KitsuneShape.png";
 
     private static final int COST = 2;
     private static final int ATTACK_DMG = 14;
