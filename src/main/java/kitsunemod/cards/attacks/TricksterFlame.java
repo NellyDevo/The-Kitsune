dfb7bdb5464b9106d601515cd22ea65e86a4d413
Rin Camelia <rcix2010@gmail.com>
2019-02-12 21:49:21 -0600

    Minor error corrections

--- src/main/java/kitsunemod/cards/attacks/TricksterFlame.java
+++ src/main/java/kitsunemod/cards/attacks/TricksterFlame.java
@@ -21,7 +21,7 @@ public class TricksterFlame extends AbstractKitsuneCard {
     public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    public static final String IMG_PATH = "kitsunemod/images/cards/NipAtHeels.png";
+    public static final String IMG_PATH = "kitsunemod/images/cards/DancingLights.png";
     private static final int COST = 1;
 
     private static final int ATTACK_DMG = 6;
