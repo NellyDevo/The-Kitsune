d1111776f646ebe43aaaca6fd7ec22f947d33465
Rin Camelia <rcix2010@gmail.com>
2019-02-17 20:21:40 -0600

    Added art for Ignite Silhouette and Inflict Wounds

--- src/main/java/kitsunemod/cards/attacks/IgniteSilhouette.java
+++ src/main/java/kitsunemod/cards/attacks/IgniteSilhouette.java
@@ -20,7 +20,7 @@ public class IgniteSilhouette extends AbstractKitsuneCard {
     public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    public static final String IMG_PATH = "kitsunemod/images/cards/strike.png";
+    public static final String IMG_PATH = "kitsunemod/images/cards/IgniteSilhouette.png";
     private static final int COST = 1;
     private static final int ATTACK_DMG = 5;
     private static final int UPGRADE_PLUS_DMG = 3;
--- src/main/java/kitsunemod/cards/attacks/InflictWounds.java
+++ src/main/java/kitsunemod/cards/attacks/InflictWounds.java
@@ -24,7 +24,7 @@ public class InflictWounds extends AbstractKitsuneCard {
     public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    public static final String IMG_PATH = "kitsunemod/images/cards/strike.png";
+    public static final String IMG_PATH = "kitsunemod/images/cards/InflictWounds.png";
     private static final int COST = 1;
 
     private static final int ATTACK_DMG = 8;
