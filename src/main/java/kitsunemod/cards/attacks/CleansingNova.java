b43dd9a51be65ae0f9adc918c22523d351628f5b
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2019-02-18 02:51:59 -0500

    fix cleansing nova to apply powers

--- src/main/java/kitsunemod/cards/attacks/CleansingNova.java
+++ src/main/java/kitsunemod/cards/attacks/CleansingNova.java
@@ -1,6 +1,10 @@
 package kitsunemod.cards.attacks;
 
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.RemoveAllBlockAction;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.actions.utility.WaitAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
@@ -10,8 +14,6 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import kitsunemod.KitsuneMod;
-import kitsunemod.actions.CleansingNovaAction;
-import kitsunemod.actions.IgniteSilhouetteAction;
 import kitsunemod.cards.AbstractKitsuneCard;
 import kitsunemod.patches.AbstractCardEnum;
 
@@ -20,17 +22,21 @@ public class CleansingNova extends AbstractKitsuneCard {
     public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String[] EXTENDED_DESCRIPTION = cardStrings.EXTENDED_DESCRIPTION;
     public static final String IMG_PATH = "kitsunemod/images/cards/CleansingNova.png";
     private static final int COST = 1;
 
     private static final int BLOCK_AMT = 7;
     private static final int UPGRADE_PLUS_BLOCK = 3;
+    private boolean doDamage = false;
 
     public CleansingNova() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
                 CardType.ATTACK, AbstractCardEnum.KITSUNE_COLOR,
                 CardRarity.RARE, CardTarget.ALL);
         block = baseBlock = BLOCK_AMT;
+        damage = baseDamage = 0;
+        isMultiDamage = true;
 
         exhaustOnUseOnce = true;
     }
@@ -39,7 +45,53 @@ public class CleansingNova extends AbstractKitsuneCard {
     public void use(AbstractPlayer p, AbstractMonster m) {
         AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, p, block));
         AbstractDungeon.actionManager.addToBottom(new WaitAction(0.1f));
-        AbstractDungeon.actionManager.addToBottom(new CleansingNovaAction(p));
+        if (doDamage) {
+            AbstractDungeon.actionManager.addToBottom(new RemoveAllBlockAction(p, p));
+            AbstractDungeon.actionManager.addToBottom(new DamageAllEnemiesAction(p, multiDamage, DamageInfo.DamageType.NORMAL, AbstractGameAction.AttackEffect.FIRE));
+            AbstractDungeon.actionManager.addToBottom(new WaitAction(0.15f));
+            AbstractDungeon.actionManager.addToBottom(new SFXAction("MONSTER_AWAKENED_ATTACK"));
+        } else {
+            p.useShakeAnimation(0.5f);
+            AbstractDungeon.actionManager.addToBottom(new SFXAction("POWER_CONSTRICTED"));
+        }
+        rawDescription = DESCRIPTION;
+        initializeDescription();
+    }
+
+    @Override
+    public void applyPowers() {
+        super.applyPowers();
+        this.baseDamage = AbstractDungeon.player.currentBlock + block;
+        if (baseDamage > 0) {
+            super.applyPowers();
+            doDamage = true;
+        } else {
+            doDamage = false;
+        }
+        rawDescription = DESCRIPTION + EXTENDED_DESCRIPTION[0];
+        initializeDescription();
+        baseDamage = 0;
+    }
+
+    @Override
+    public void calculateCardDamage(AbstractMonster mo) {
+        super.calculateCardDamage(mo);
+        this.baseDamage = AbstractDungeon.player.currentBlock + block;
+        if (baseDamage > 0) {
+            super.calculateCardDamage(mo);
+            doDamage = true;
+        } else {
+            doDamage = false;
+        }
+        rawDescription = DESCRIPTION + EXTENDED_DESCRIPTION[0];
+        initializeDescription();
+        baseDamage = 0;
+    }
+
+    @Override
+    public void onMoveToDiscard() {
+        rawDescription = DESCRIPTION;
+        initializeDescription();
     }
 
     @Override
