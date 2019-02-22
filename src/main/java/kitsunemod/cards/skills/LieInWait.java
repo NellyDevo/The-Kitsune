02811f8df3877a3b043054c8d54798ced5b85aea
Rin Camelia <rcix2010@gmail.com>
2019-02-22 13:28:27 -0600

    Added Newline to Lie in Wait

--- src/main/java/kitsunemod/cards/skills/LieInWait.java
+++ src/main/java/kitsunemod/cards/skills/LieInWait.java
@@ -66,15 +66,25 @@ public class LieInWait extends AbstractKitsuneCard {
 
     private String getRawDescription() {
         String description = DESCRIPTION;
-        for (int i = 0; i < secondMagicNumber; i++) {
-            description += cardStrings.EXTENDED_DESCRIPTION[0];
+        String energyString = "";
+        if (secondMagicNumber == 1) {
+            energyString += cardStrings.EXTENDED_DESCRIPTION[0];
+            energyString += cardStrings.EXTENDED_DESCRIPTION[2];
         }
+        else if (secondMagicNumber > 1) {
+            energyString += cardStrings.EXTENDED_DESCRIPTION[0];
+            for (int i = 0; i < secondMagicNumber - 1; i++) {
+                energyString += cardStrings.EXTENDED_DESCRIPTION[1];
+            }
+            energyString += cardStrings.EXTENDED_DESCRIPTION[2];
+        }
+        description += energyString;
         if (!upgraded) {
-            description += cardStrings.EXTENDED_DESCRIPTION[1];
+            description += cardStrings.EXTENDED_DESCRIPTION[2];
         }
         else
         {
-            description += cardStrings.EXTENDED_DESCRIPTION[2];
+            description += cardStrings.EXTENDED_DESCRIPTION[3];
         }
         return description;
     }
