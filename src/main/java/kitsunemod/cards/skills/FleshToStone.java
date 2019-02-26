7265c860cb18e6763553aa0b1f14a108f58078fc
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2019-02-26 16:39:12 -0500

    Flesh to Stone art

--- src/main/java/kitsunemod/cards/skills/FleshToStone.java
+++ src/main/java/kitsunemod/cards/skills/FleshToStone.java
@@ -23,7 +23,7 @@ public class FleshToStone extends AbstractKitsuneCard {
     public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    public static final String IMG_PATH = "kitsunemod/images/cards/default_skill.png";
+    public static final String IMG_PATH = "kitsunemod/images/cards/FleshToStone.png";
     private static final int COST = 1;
 
     private static final int BLOCK_AMT = 14;
