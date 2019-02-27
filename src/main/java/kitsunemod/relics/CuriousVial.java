627654a0037b64dfda051f6e6f16144e9b7605b1
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2019-02-27 02:39:34 -0500

    fix curious vial ID

--- src/main/java/kitsunemod/relics/CuriousVial.java
+++ src/main/java/kitsunemod/relics/CuriousVial.java
@@ -2,7 +2,6 @@ package kitsunemod.relics;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.evacipated.cardcrawl.mod.stslib.relics.ClickableRelic;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
@@ -10,7 +9,7 @@ import kitsunemod.KitsuneMod;
 import kitsunemod.actions.ApplyDarkAction;
 
 public class CuriousVial extends KitsuneRelic implements ClickableRelic {
-    public static final String ID = KitsuneMod.makeID("BrokenCollar");
+    public static final String ID = KitsuneMod.makeID("CuriousVial");
     public static final Texture IMG = new Texture("kitsunemod/images/relics/starterrelic.png");
     public static final Texture OUTLINE = new Texture("kitsunemod/images/relics/starterrelic_p.png");
 
