f61207fa592ffeef53f362328df500aa680de8e9
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2019-02-27 02:58:08 -0500

    get rid of copy-paste art

--- src/main/java/kitsunemod/cards/attacks/DeathStroke.java
+++ src/main/java/kitsunemod/cards/attacks/DeathStroke.java
@@ -20,7 +20,7 @@ public class DeathStroke extends AbstractKitsuneCard {
     public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
-    public static final String IMG_PATH = "kitsunemod/images/cards/CleansingNova.png";
+    public static final String IMG_PATH = "kitsunemod/images/cards/default_attack.png";
     private static final int COST = 1;
 
     private static final int DAMAGE_AMT = 12;
