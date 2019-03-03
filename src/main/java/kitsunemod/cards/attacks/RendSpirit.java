7eb59c1fcf72e15a43088f26f9db9d0abd05f2da
Rin Camelia <rcix2010@gmail.com>
2019-03-03 15:06:22 -0600

    Redesign Rend Spirit to be an HP loss damage attack

--- src/main/java/kitsunemod/cards/attacks/RendSpirit.java
+++ src/main/java/kitsunemod/cards/attacks/RendSpirit.java
@@ -19,6 +19,7 @@ import kitsunemod.patches.AbstractCardEnum;
 import kitsunemod.patches.KitsuneTags;
 import kitsunemod.powers.KitsuneShapePower;
 import kitsunemod.powers.NinetailedShapePower;
+import kitsunemod.powers.SoulstealPower;
 
 public class RendSpirit extends AbstractKitsuneCard {
     public static final String ID = KitsuneMod.makeID("RendSpirit");
@@ -28,29 +29,33 @@ public class RendSpirit extends AbstractKitsuneCard {
     public static final String IMG_PATH = "kitsunemod/images/cards/default_attack.png";
     private static final int COST = 3;
 
-    private static final int ATTACK_DMG = 20;
-    private static final int UPGRADE_PLUS_DMG = 5;
+    private static final int ATTACK_DMG = 35;
+    private static final int UPGRADE_PLUS_DMG = 10;
 
-    private static final int ASPECT_HP_LOSS = 10;
-    private static final int UPGRADE_PLUS_ASPECT_HP_LOSS = 5;
+    private static final int BASE_HP_LOSS = 10;
+    private static final int HP_LOSS_REDUCTION_MULTIPLIER = 1;
 
     public RendSpirit() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
                 CardType.ATTACK, AbstractCardEnum.KITSUNE_COLOR,
                 CardRarity.COMMON, CardTarget.ENEMY);
         damage = baseDamage = ATTACK_DMG;
-        magicNumber = baseMagicNumber = ASPECT_HP_LOSS;
-        tags.add(KitsuneTags.ASPECT_CARD);
+        magicNumber = baseMagicNumber = BASE_HP_LOSS;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SMASH));
-        if (p.hasPower(KitsuneShapePower.POWER_ID) || p.hasPower(NinetailedShapePower.POWER_ID)) {
-            AbstractDungeon.actionManager.addToBottom(new WaitAction(0.75f));
-            AbstractDungeon.actionManager.addToBottom(new VFXAction(new ScrapeEffect(m.hb.cX, m.hb.cY)));
-            AbstractDungeon.actionManager.addToBottom(new WaitAction(0.25f));
-            AbstractDungeon.actionManager.addToBottom(new LoseHPAction(m, p, magicNumber));
+        AbstractDungeon.actionManager.addToBottom(new VFXAction(new ScrapeEffect(m.hb.cX, m.hb.cY)));
+        AbstractDungeon.actionManager.addToBottom(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn)));
+        int hpLoss = magicNumber;
+        if (m.hasPower(SoulstealPower.POWER_ID)) {
+            hpLoss -= m.getPower(SoulstealPower.POWER_ID).amount * HP_LOSS_REDUCTION_MULTIPLIER;
+        }
+        if (hpLoss < 0) {
+            hpLoss = 0;
+        }
+        if (hpLoss > 0) {
+            AbstractDungeon.actionManager.addToBottom(new LoseHPAction(p, p, hpLoss));
         }
     }
 
@@ -64,7 +69,6 @@ public class RendSpirit extends AbstractKitsuneCard {
         if (!upgraded) {
             upgradeName();
             upgradeDamage(UPGRADE_PLUS_DMG);
-            upgradeMagicNumber(UPGRADE_PLUS_ASPECT_HP_LOSS);
         }
     }
 }
