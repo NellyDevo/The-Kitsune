268b942fc6066d21339febd7bb4c15e270118e81
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2019-03-24 08:56:23 -0400

    rework ancient wisdom

--- src/main/java/kitsunemod/actions/AncientWisdomAction.java
+++ src/main/java/kitsunemod/actions/AncientWisdomAction.java
@@ -1,40 +1,17 @@
 package kitsunemod.actions;
 
+import com.evacipated.cardcrawl.mod.stslib.actions.tempHp.AddTemporaryHPAction;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 
-import java.util.ArrayList;
-
 public class AncientWisdomAction extends AbstractGameAction {
-
-    public AncientWisdomAction(int amount) {
-        this.amount = amount;
-    }
     
     @Override
     public void update() {
         AbstractPlayer p = AbstractDungeon.player;
-        int tmp = p.hand.size();
-        ArrayList<AbstractCard> list = new ArrayList<>();
-        if (!(amount >= tmp)) {
-            for (int i = 0; i < amount; ++i) {
-                AbstractCard randomCard = p.hand.getRandomCard(AbstractDungeon.cardRandomRng);
-                while (list.contains(randomCard)) {
-                    randomCard = p.hand.getRandomCard(AbstractDungeon.cardRandomRng);
-                }
-                list.add(randomCard);
-            }
-        } else {
-            list.addAll(p.hand.group);
-        }
-        int lightAmount = 0;
-        for (AbstractCard card : list) {
-            card.flash();
-            lightAmount += card.costForTurn;
-        }
-        AbstractDungeon.actionManager.addToTop(new ApplyLightAction(p, p, lightAmount));
-        isDone = true;
+        int tmp = p.currentBlock;
+        p.loseBlock();
+        AbstractDungeon.actionManager.addToTop(new AddTemporaryHPAction(p, p, tmp));
     }
 }
